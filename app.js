5eb859a2c4949ffc99e5d2cffb9a93e63d884697
Vivek <vivk84505@gmail.com>
2024-04-13 14:23:49 +0530

    test commit

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 // Import required modules
-const express = require('express');
+const express = require("express");
 
 // Create an Express application
 const app = express();
 
 // Define routes
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
+app.get("/", (req, res) => {
+  res.send("Welcome to Real Estate CRM!");
 });
 
 // Start the server
 const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
